bff8a37d858b66979477cd8e1b37138cd1fdbc07
Erik Unger <erik@erikunger.com>
2018-11-10 17:29:06 +0100

    added FlatStructFieldCount, FlatStructFieldNames, FlatStructFieldValues

--- reflection.go
+++ reflection.go
@@ -222,3 +222,59 @@ func ValuesToInterfaces(values ...reflect.Value) []interface{} {
 	}
 	return s
 }
+
+// FlatStructFieldCount returns the number of flattened struct fields,
+// meaning that the fields of anonoymous embedded fields are flattened
+// to the top level of the struct.
+func FlatStructFieldCount(t reflect.Type) int {
+	t = DerefType(t)
+	count := 0
+	numField := t.NumField()
+	for i := 0; i < numField; i++ {
+		f := t.Field(i)
+		if f.Anonymous {
+			count += FlatStructFieldCount(f.Type)
+		} else {
+			count++
+		}
+	}
+	return count
+}
+
+// FlatStructFieldNames returns the names of flattened struct fields,
+// meaning that the fields of anonoymous embedded fields are flattened
+// to the top level of the struct.
+func FlatStructFieldNames(t reflect.Type) (names []string) {
+	t = DerefType(t)
+	numField := t.NumField()
+	names = make([]string, 0, numField)
+	for i := 0; i < numField; i++ {
+		f := t.Field(i)
+		if f.Anonymous {
+			names = append(names, FlatStructFieldNames(f.Type)...)
+		} else {
+			names = append(names, f.Name)
+		}
+	}
+	return names
+}
+
+// FlatStructFieldValues returns the values of flattened struct fields,
+// meaning that the fields of anonoymous embedded fields are flattened
+// to the top level of the struct.
+func FlatStructFieldValues(v reflect.Value) (values []reflect.Value) {
+	v = DerefValue(v)
+	t := v.Type()
+	numField := t.NumField()
+	values = make([]reflect.Value, 0, numField)
+	for i := 0; i < numField; i++ {
+		ft := t.Field(i)
+		fv := v.Field(i)
+		if ft.Anonymous {
+			values = append(values, FlatStructFieldValues(fv)...)
+		} else {
+			values = append(values, fv)
+		}
+	}
+	return values
+}
